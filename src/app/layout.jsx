4c214079d3ca9a5f7002b8f0ed8d0d4eb60d6ede
Riya kaushik <riyakaushik6410@gmail.com>
2025-07-15 12:04:20 +0530

    fix: fixed import issue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar} from "../components/navbar"
 import {Footer} from "../components/footer"
-import Loader from "@/components/Loader";
+import Loader from "../components/loader";
 import { useState, useEffect } from "react";
 
 export default function RootLayout({ children }) {
